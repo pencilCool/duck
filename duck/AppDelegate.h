49f9aa9bae1bb71db91e189086e9a08af169fd97
pencilCool <yhtang_xt@163.com>
2017-03-17 17:13:45 +0800

    Initial Commit

---
+++ duck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  duck
+//
+//  Created by tangyuhua on 2017/3/17.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ duck/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  duck
+//
+//  Created by tangyuhua on 2017/3/17.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
