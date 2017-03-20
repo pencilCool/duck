4d031d55e955f7aca8130e33ae0f27c54b71f147
pencilCool <yhtang_xt@163.com>
2017-03-20 12:49:44 +0800

    first commit

---
+++ duck/XXDIProxy.h
@@ -0,0 +1,13 @@
+//
+//  XXDIProxy.h
+//  duck
+//
+//  Created by tangyuhua on 2017/3/20.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XXDIProxy : NSProxy
+
+@end
---
+++ duck/XXDIProxy.m
@@ -0,0 +1,13 @@
+//
+//  XXDIProxy.m
+//  duck
+//
+//  Created by tangyuhua on 2017/3/20.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+#import "XXDIProxy.h"
+
+@implementation XXDIProxy
+
+@end
